000e1d68dcc9b2a852a4e2325b6b4158a643c8ec
Joe Chen <joe@sourcegraph.com>
2020-08-06 15:01:51 +0800

    Fix wrong columns in down script

--- migrations/1528395699_alter_permissions_object_ids_to_intarray.down.sql
+++ migrations/1528395699_alter_permissions_object_ids_to_intarray.down.sql
@@ -11,10 +11,10 @@ ALTER TABLE repo_pending_permissions DROP COLUMN IF EXISTS user_ids;
 ALTER TABLE user_pending_permissions DROP COLUMN IF EXISTS object_ids;
 
 -- Reset values to be binary format of empty roaring bitmaps
-ALTER TABLE user_permissions ADD COLUMN object_ids BYTEA NOT NULL DEFAULT '\x3a30000000000000';
 ALTER TABLE repo_permissions ADD COLUMN user_ids BYTEA NOT NULL DEFAULT '\x3a30000000000000';
-ALTER TABLE repo_pending_permissions ADD COLUMN object_ids BYTEA NOT NULL DEFAULT '\x3a30000000000000';
-ALTER TABLE user_pending_permissions ADD COLUMN user_ids BYTEA NOT NULL DEFAULT '\x3a30000000000000';
+ALTER TABLE user_permissions ADD COLUMN object_ids BYTEA NOT NULL DEFAULT '\x3a30000000000000';
+ALTER TABLE repo_pending_permissions ADD COLUMN user_ids BYTEA NOT NULL DEFAULT '\x3a30000000000000';
+ALTER TABLE user_pending_permissions ADD COLUMN object_ids BYTEA NOT NULL DEFAULT '\x3a30000000000000';
 
 DROP EXTENSION IF EXISTS intarray;
 
